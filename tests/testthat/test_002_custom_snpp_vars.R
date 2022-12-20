bf592ab1b547d69df1276f126311ab354245b95f
paulseamer <paulseamer@nhs.net>
2022-12-20 13:29:10 +0000

    update tests for new custom variants method

--- tests/testthat/test_002_custom_snpp_vars.R
+++ tests/testthat/test_002_custom_snpp_vars.R
@@ -1,8 +1,47 @@
 
-test_that("custom sub-national vars total equal npp vars total", {
-  expect_equal(
-    map(ls_snpp_custom_vars, ~ . |> group_by(year) |> summarise(pop = sum(pop))),
-    map(npp_2018b_dat, ~ . |> filter(year %in% as.character(2018:2043)) |> group_by(year) |> summarise(pop = sum(pop))) 
+
+test_that("test consistency of custom variant order v. NPP order", {
+  expect_lte(
+    max(abs(
+      rnk_diffs |> 
+        pull(diff_rnk)
+    )), 2L
   )
 })
 
+test_that("test consistency of custom variant order v. NPP order", {
+  expect_gt(
+    max(abs(
+      rnk_diffs |> 
+        group_by(abs_diff_rnk = abs(diff_rnk)) |> 
+        summarise(wght_diff = sum(n)) |> 
+        filter(abs_diff_rnk == 0) |> 
+        pull(wght_diff)
+    )), .8 * (18 * 326)
+  )
+})
+
+test_that("test difference between variants that are in both SNPP and NPP, by LA", {
+  expect_lte(
+    max(abs(
+      cf_vars |> 
+        pull(p_high)
+    )), 10L
+  )
+  expect_lte(
+    max(abs(
+      cf_vars |> 
+        pull(p_low)
+    )), 10L
+  )
+  expect_lte(
+    cf_vars |> 
+      summarise(p_high = mean(abs(p_high)))
+    , 1L
+  )
+  expect_lte(
+    cf_vars |> 
+      summarise(p_low = mean(abs(p_low)))
+    , 1L
+  )
+})
