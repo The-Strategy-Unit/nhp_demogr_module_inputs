a74766320abc802b6ed4525b618e54ca59791838
paulseamer <paulseamer@nhs.net>
2023-10-12 10:27:56 +0100

    Read-in npp 2018b births by age of mother

---
+++ read-npp-2018b-births.R
@@ -0,0 +1,40 @@
+# README ----
+# Read in national population projections (2018-based)
+# births by age of mother (sheet in main population file)
+# x17 variant projections were published alongside the principal
+
+
+# packages ----
+library("dplyr")
+library("here")
+library("purrr")
+library("readxl")
+library("readr")
+library("stringr")
+library("tidyr")
+
+# read npp 2018b births ----
+read_npp_birth_files <- function(dir) {
+  npp_files <- list.files(
+    here(dir),
+    "2018(.xls)$",
+    recursive = TRUE,
+    full.names = TRUE
+  )
+  id <- str_extract(npp_files, "(?<=npp_2018b/).*(?=_opendata)")
+  l <- map(npp_files, ~ readxl::read_xls(.x, sheet = "Births")) |>
+    setNames(id)
+  bind_rows(l, .id = "id") |>
+    rename_all(tolower) |>
+    pivot_longer(
+      cols = `2018 - 2019`:`2117 - 2118`,
+      names_to = "year",
+      names_prefix = "[0-9]{4} - ",
+      values_to = "pop"
+    ) |>
+    mutate(age = str_trim(age), area = "England", pop = as.double(pop)) |>
+    mutate(sex = ifelse(sex == "1", "m", "f")) |>
+    write_rds(here("data", "npp_2018b_births.rds"))
+}
+
+read_npp_birth_files("data-raw")
