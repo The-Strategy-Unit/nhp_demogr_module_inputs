72258d2df5a9baf7b8abd0b428f3e86e1c3c2823
paulseamer <paulseamer@nhs.net>
2023-10-13 20:52:25 +0100

    Minor refactoring

--- tests/testthat/test-model-syoa-100-snpp-2018b.R
+++ tests/testthat/test-model-syoa-100-snpp-2018b.R
@@ -1,28 +1,40 @@
-
-snpp_90 <- out_dat |> 
-  mutate(data = map(data, \(x)
-                    x |>
-                      filter(str_detect(area_code, "^E07", negate = TRUE)) |> 
-                      filter(age >= 90) |> 
-                      group_by(year) |> 
-                      summarise(pop = sum(pop)) |> 
-                      ungroup()
-  )) |> 
+# README ----
+# 1 test for each of the x4 snpp variants that the sum of modeled 90 to 100+
+# population across all LAs is the same as the 90+ population for England
+# from NPP
+snpp_90 <- out_dat |>
+  mutate(data = map(
+    data, \(x) x |>
+      filter(str_detect(area_code, "^E07", negate = TRUE)) |>
+      filter(age >= 90) |>
+      group_by(year) |>
+      summarise(pop = sum(pop)) |>
+      ungroup()
+  )) |>
   unnest(data)
 
-npp_90 <- npp_dat |> 
-  filter(age %in% c(as.character(seq(90, 104)), "105 - 109", "110 and over")) |> 
-  filter(year <= 2043) |> 
-  group_by(id, year) |> 
-  summarise(pop = sum(pop)) |> 
-  ungroup()
+npp_90 <- npp_dat |>
+  mutate(data = map(
+    data, \(x) x |>
+      filter(age_group %in% c(as.character(seq(90, 104)),
+          "105 - 109", "110 and over"
+        )
+      ) |>
+      filter(year %in% as.character(2018:2043)) |>
+      group_by(year) |>
+      summarise(pop = sum(pop)) |>
+      ungroup()
+  )) |>
+  unnest(data)
 
-test_that("spread snpp 90+ populations match original npp 90+ populations by variant", {
-  expect_lt(npp_90 |> 
-              inner_join(snpp_90, join_by("id" == "map_id", "year")) |> 
-              mutate(diff = pop.x - pop.y) |> 
-              summarise(diff = sum(abs(diff))) |> 
-              pull(diff)
-    , 1)
-})
-
+test_that("modeled snpp variant 90-100+ populations match
+  npp 90+ populations", {
+    expect_lt(
+      npp_90 |>
+        inner_join(snpp_90, join_by("ons_id" == "ons_id.y", "year")) |>
+        mutate(diff = pop.x - pop.y) |>
+        summarise(diff = sum(abs(diff))) |>
+        pull(diff), 1
+    )
+  }
+)
