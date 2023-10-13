cb03896a83c640e1324ef39e512850b57e2c32e8
paulseamer <paulseamer@nhs.net>
2023-10-13 16:25:44 +0100

    Minor refactoring

--- read-snpp-2018b-births.R
+++ read-snpp-2018b-births.R
@@ -1,6 +1,6 @@
 # README ----
-# Read in sub-national population projections (2018-based)
-# births by age of mother (SNPP Z3)
+# Read-in sub-national population projections (2018-based)
+# births by age of mother (snpp Z3)
 # x4 variant projections were published alongside the principal
 
 
@@ -23,16 +23,21 @@ read_snpp_birth_files <- function(dir) {
   id <- str_match(snpp_files, "2018b_(.*?)/2018")[, 2]
   l <- map(snpp_files, ~ read_csv(.x)) |>
     setNames(id)
-  bind_rows(l, .id = "id") |>
+  bind_rows(l, .id = "ons_id") |>
     rename_all(tolower) |>
     # first year of births is 2019
     pivot_longer(cols = `2019`:`2043`, names_to = "year", values_to = "bths") |>
     filter(age_group != "All ages") |>
     rename(age = age_group) |>
     select(-component) |>
-    mutate(sex = str_sub(sex, 1L, 1L)) |>
+    mutate(
+      age = as.integer(age),
+      sex = str_sub(sex, 1L, 1L),
+      ons_id = str_remove(ons_id, "_births")
+    ) |>
     # remove metropolitan counties and regions (keep only local authorities)
     filter(str_detect(area_code, "^E11|^E12", negate = TRUE)) |>
+    nest(data = c(everything(), -ons_id)) |>
     write_rds(here("data", "snpp_2018b_births.rds"))
 }
 
