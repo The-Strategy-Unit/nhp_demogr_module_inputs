1a6edb7d47a89367632e5da68a6264af77115ffa
paulseamer <paulseamer@nhs.net>
2023-10-13 16:36:14 +0100

    Minor refactoring

--- read-npp-2018b.R
+++ read-npp-2018b.R
@@ -1,45 +1,47 @@
-
 # README ----
-# Read in national population projections (2018-based)
+# Read-in national population projections (2018-based)
 # x17 variant projections were published alongside the principal
 
 
 # packages ----
-{
-  library("dplyr")
-  library("here")
-  library("purrr")
-  library("readxl")
-  library("stringr")
-  library("tidyr")
-}
-
+library("dplyr")
+library("here")
+library("purrr")
+library("readr")
+library("readxl")
+library("stringr")
+library("tidyr")
 
 # read npp 2018b ----
 change_file_ext <- function(x) {
   system2("powershell", args = c("-file", x))
 }
 # only need to run this once to change file extension
-# change_file_ext("cmd_line_shithousery.ps1")
+# change_file_ext("cmd-line-dark-arts.ps1")
 
 read_npp_files <- function(dir) {
   npp_files <- list.files(
-    here::here(
-      dir
-    ),
+    here(dir),
     "2018(.xls)$",
     recursive = TRUE,
     full.names = TRUE
   )
-  id <- stringr::str_extract(npp_files, "(?<=npp_2018b/).*(?=_opendata)")
-  l <- purrr::map(npp_files, ~ readxl::read_xls(.x, sheet = "Population")) |>
+  id <- str_extract(npp_files, "(?<=npp_2018b/).*(?=_opendata)")
+  l <- map(npp_files, ~ read_xls(.x, sheet = "Population")) |>
     setNames(id)
-  dat <- dplyr::bind_rows(l, .id = "id") |>
-    dplyr::rename_all(tolower) |>
-    tidyr::pivot_longer(cols = `2018`:`2118`, names_to = "year", values_to = "pop") |>
-    dplyr::mutate(age = stringr::str_trim(age), area = "England", pop = as.double(pop)) |>
-    dplyr::mutate(sex = ifelse(sex == "1", "m", "f")) |> 
-    saveRDS(here("data", "npp_2018b.rds"))
+  dat <- bind_rows(l, .id = "ons_id") |>
+    rename_all(tolower) |>
+    pivot_longer(cols = `2018`:`2118`, names_to = "year", values_to = "pop") |>
+    mutate(
+      age = str_trim(age),
+      area = "England",
+      pop = as.double(pop),
+      sex = ifelse(sex == "1", "m", "f"),
+      ons_id = str_replace(ons_id, "en_", "npp_")
+    ) |>
+    rename(age_group = age) |>
+    nest(data = c(everything(), -ons_id)) |>
+    write_rds(here("data", "npp_2018b.rds"))
 }
 
 read_npp_files("data-raw")
