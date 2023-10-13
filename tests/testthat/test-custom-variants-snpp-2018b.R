c8d0c3799feee220ee47a3c393a14d4442842d14
paulseamer <paulseamer@nhs.net>
2023-10-13 20:51:20 +0100

    Minor refactoring

--- tests/testthat/test-custom-variants-snpp-2018b.R
+++ tests/testthat/test-custom-variants-snpp-2018b.R
@@ -1,90 +1,104 @@
-
-x <- read_rds(here("data", "snpp_2018b_custom_vars.rds"))
-y <- read_rds(here("data", "npp_2018b.rds"))
-z <- read_rds(here("data", "snpp_2018b_syoa_100.rds"))
-
-# A) test consistency of custom variant total population ranking v. npp rank by LA
+# README ----
+# 1 rank custom variants by total population within each LA
+# (for year == test_yr) and test against ranking of npp variants for total
+# population in England
 # pick a year to test
 test_yr <- "2035"
 
-cus_rnk <- custom |> 
-  mutate(data = map(data, \(x) x |> 
-                      filter(year == test_yr) |> 
-                      filter(str_detect(area_code, "^E07", negate = TRUE)) |>
-                      group_by(area_code, area_name) |> 
-                      summarise(pop = sum(pop)) |> 
-                      ungroup()
-                    )
-  ) |> 
-  unnest(data) |> 
+cus_rnk <- custom |>
+  mutate(data = map(
+    data, \(x) x |>
+      filter(year == test_yr) |>
+      filter(str_detect(area_code, "^E07", negate = TRUE)) |>
+      group_by(area_code, area_name) |>
+      summarise(pop = sum(pop)) |>
+      ungroup()
+  )) |>
+  unnest(data) |>
   group_by(area_code, area_name) |>
-  arrange(pop) |> 
-  mutate(cus_rnk = 1:n()) |> 
+  arrange(pop) |>
+  mutate(cus_rnk = seq_len(n())) |>
   ungroup()
 
-npp_rnk <- npp_dat |> 
-  filter(year == test_yr) |> 
-  group_by(id) |> 
-  summarise(pop = sum(pop)) |> 
-  ungroup() |> 
+npp_rnk <- npp_dat |>
+  unnest(c(data)) |>
+  filter(year == test_yr) |>
+  group_by(ons_id) |>
+  summarise(pop = sum(pop)) |>
+  ungroup() |>
   arrange(pop) |>
-  mutate(npp_rnk = 1:n()) |> 
-  select(id, npp_rnk)
+  mutate(npp_rnk = seq_len(n())) |>
+  select(ons_id, npp_rnk)
 
 rnk_diff <- cus_rnk |>
-  left_join(npp_rnk, join_by("id")) |> 
+  left_join(npp_rnk, join_by("ons_id")) |>
   mutate(rnk_diff = cus_rnk - npp_rnk) |>
   arrange(rnk_diff)
 
-test_that("test consistency of custom variant ranking v. NPP rank by LA", {
-  expect_lte(
-    max(abs(rnk_diff$rnk_diff)), 3L
+test_that("test consistency of custom variant ranking within LAs against NPP
+  ranking", {
+    expect_lte(
+      max(abs(rnk_diff$rnk_diff)), 3L
     )
-})
+  }
+)
 
+# 2 compare total population by LA (for year == test_yr) for custom variants
+# that also appear in original snpp variant set
+test_yr <- "2040"
 
-# B) test difference between population totals for variants that are in both custom vars and original snpp set by LA
-cus_vars <- custom |> 
-  filter(id %in% c("en_pph", "en_ppl")) |>
-  mutate(data = map(data, \(x) x |> 
-                      filter(year == test_yr) |> 
-                      group_by(area_code, area_name) |> 
-                      summarise(pop = sum(pop)) |> 
-                      ungroup()
+cus_vars <- custom |>
+  filter(ons_id %in% c("npp_pph", "npp_ppl")) |>
+  mutate(data = map(
+    data, \(x) x |>
+      filter(year == test_yr) |>
+      group_by(area_code, area_name) |>
+      summarise(pop = sum(pop)) |>
+      ungroup()
+  )) |>
+  unnest(data) |>
+  select(-id) |>
+  left_join(
+    lookup_proj_var |>
+      select(ons_id, id),
+    join_by("ons_id")
   )
-  ) |> 
-  unnest(data)
 
 orig_snpp <- snpp_dat |>
   filter(ons_id %in% c(
     "var_proj_low_intl_migration",
-    "var_proj_high_intl_migration")
-  ) |> 
-  mutate(data = map(data, \(x) x |> 
-                      filter(year == test_yr) |>
-                      group_by(area_code, area_name) |> 
-                      summarise(pop = sum(pop)) |> 
-                      ungroup()
-  )
-  ) |> 
+    "var_proj_high_intl_migration"
+  )) |>
+  mutate(data = map(
+    data, \(x) x |>
+      filter(year == test_yr) |>
+      group_by(area_code, area_name) |>
+      summarise(pop = sum(pop)) |>
+      ungroup()
+  )) |>
   unnest(data)
 
-var_diff <-cus_vars |> 
-  left_join(orig_snpp, join_by("id" == "map_id", "area_code", "area_name")) |> 
-  mutate(diff = (pop.x / pop.y - 1) * 100) |> 
+var_diff <- cus_vars |>
+  left_join(orig_snpp, join_by("id", "area_code", "area_name")) |>
+  mutate(diff = (pop.x / pop.y - 1) * 100) |>
   arrange(diff)
 
 # plot differences
-# ggplot(data = var_diff, aes(x = reorder(area_name, diff), y = diff, group = 1L)) +
-#   geom_point() +
-#   scale_x_discrete(name = NULL) +
-#   scale_y_continuous(name = NULL) +
-#   facet_wrap(vars(id)) +
-#   theme(axis.text.x = element_text(angle = 90, hjust = 1, size = rel(.8)))
+p  <- var_diff |>
+  mutate(area_name = tidytext::reorder_within(area_name, diff, id)) |>
+  ggplot(aes(x = reorder(area_name, diff), y = diff, group = 1L)) +
+  geom_point() +
+  scale_x_reordered(name = NULL) +
+  scale_y_continuous(name = NULL) +
+  facet_wrap(vars(id)) +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, size = rel(.8)))
 
-test_that("test difference between variants that are in both custom variants and origianl SNPP set by LA", {
-  expect_lte(
-    max(abs(var_diff$diff)), 10
-  )
-})
+ggsave("test_custom_variants_snpp.png", plot = p, path = here("figures"))
 
+test_that("test difference by LA for custom variants that also appear in
+  original snpp variant set", {
+    expect_lte(
+      mean(abs(var_diff$diff)), 2
+    )
+  }
+)
