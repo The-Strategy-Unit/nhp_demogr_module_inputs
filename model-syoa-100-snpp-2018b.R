945e90f4985193c369b582506c211dcb37263272
paulseamer <paulseamer@nhs.net>
2023-10-13 16:51:52 +0100

    Minor refactoring

--- model-syoa-100-snpp-2018b.R
+++ model-syoa-100-snpp-2018b.R
@@ -1,25 +1,20 @@
-
 # README ----
-# SNPP (2018-based) stop at age 90+
-# NPP (2018-based) stop at age 104 (105-109, 110+)
-# We take the 90+ distribution from NPP and apply it to SNPP to obtain a dataset
-# for ages 0-100+
-# Decision: which NPP variant should we take the 90+ distribution come from?
-# Ideally, there exists a 1:1 mapping of SNPP variants to NPP variants,
-# however no mapping exists, so we need to create one
+# snpp (2018-based) ends at age 90+; npp (2018-based) ends at age 104,
+# (105-109, 110+). We take the 90+ distribution from npp and apply it to snpp to
+# obtain a dataset for ages 0-100+.
+# Q. Which NPP variant should we take the 90+ distribution from?
+# ideally, there exists a 1:1 mapping of snpp variants to npp variants,
+# however no mapping exists, so we must create one (see create-lookups.R)
 
 
 # packages ----
-{
-  library("dplyr")
-  library("here")
-  library("purrr")
-  library("readr")
-  library("stringr")
-  library("testthat")
-  library("tidyr")
-}
-
+library("dplyr")
+library("here")
+library("purrr")
+library("readr")
+library("stringr")
+library("testthat")
+library("tidyr")
 
 # 1 ----
 apply_90plus_dist <- function(npp, snpp) {
@@ -29,78 +24,84 @@ apply_90plus_dist <- function(npp, snpp) {
   lookup_proj_var <- read_csv(here("data", "lookup_proj_var.csv"))
 
   # extract 90+ distribution from npp
-  npp_plus90 <- npp_dat |>
-    dplyr::group_by(id) |>
-    tidyr::nest() |>
-    dplyr::mutate(
-      npp_p90 = purrr::map(
-        data, ~ .x |>
-          dplyr::filter(stringr::str_detect(age, "^1[0-1][0-9]|^9[0-9]")) |>
-          dplyr::mutate(
-            age =
-              case_when(
-                age %in% c(
-                  "100", "101", "102", "103", "104",
-                  "105 - 109", "110 and over"
-                ) ~ "100",
-                TRUE ~ as.character(age)
-              )
-          ) |>
-          dplyr::group_by(dplyr::across(-c(pop))) |>
-          dplyr::summarise(pop = sum(pop)) |>
-          dplyr::group_by(dplyr::across(-c(age, pop))) |>
-          dplyr::mutate(pop_pct = pop / sum(pop)) |>
-          dplyr::ungroup() |>
-          dplyr::mutate(age = as.integer(age)) |>
-          dplyr::select(year, sex, age, pop_pct)
-      )
-    ) |>
-    dplyr::filter(id %in% unique(var_map$map_id)) |>
-    dplyr::select(-data) |> 
-    dplyr::rename(map_id = id)
-  
+  npp_p90 <- npp_dat |>
+    mutate(npp_p90 = map(
+      data, \(x) x |>
+        filter(str_detect(age_group, "^1[0-1][0-9]|^9[0-9]")) |>
+        mutate(age_group = case_when(
+          age_group %in% c("100", "101", "102", "103", "104",
+            "105 - 109", "110 and over"
+          ) ~ "100",
+          TRUE ~ age_group
+        ),
+        age_group = as.integer(age_group)
+        ) |>
+        rename(age = age_group) |>
+        group_by(across(-c(pop))) |>
+        summarise(pop = sum(pop)) |>
+        group_by(across(-c(age, pop))) |>
+        mutate(pop_pct = pop / sum(pop)) |>
+        ungroup() |>
+        select(year, sex, age, pop_pct)
+    )) |>
+    filter(ons_id %in% c("npp_ppp", "npp_pph", "npp_ppl")) |>
+    select(-data) |>
+    left_join(
+      lookup_proj_var |>
+        select(ons_id, id),
+      join_by("ons_id")
+    )
+
   # apply npp 90+ distribution to snpp
   out_dat <- snpp_dat |>
-    dplyr::rename(ons_id = id) |>
-    dplyr::group_by(ons_id) |>
-    tidyr::nest() |>
-    dplyr::mutate(snpp_p90 = purrr::map(data, ~ .x |>
-                                          dplyr::filter(age_group == "90 and over") |>
-                                          dplyr::mutate(age = 90L) %>%
-                                          dplyr::select(-age_group) |>
-                                          dplyr::group_by(area_code, area_name, sex, year) |>
-                                          tidyr::complete(age = 90:100))) |>
-    dplyr::left_join(var_map, by = "ons_id") |>
-    dplyr::left_join(npp_plus90, by = "map_id") |>
-    dplyr::mutate(snpp_p90 = map2(
-      snpp_p90, npp_p90, ~ .x |>
-        dplyr::left_join(.y, by = c("year", "sex", "age")) |>
-        dplyr::group_by(area_code, area_name, sex, year) |>
-        dplyr::mutate(pop = pop_pct * sum(pop, na.rm = TRUE)) |>
-        dplyr::ungroup() |>
-        dplyr::select(-pop_pct)
+    mutate(snpp_p90 = map(
+      data, \(x) x |>
+        filter(age_group == "90 and over") |>
+        mutate(age_group = 90L) %>%
+        rename(age = age_group) |>
+        group_by(area_code, area_name, sex, year) |>
+        tidyr::complete(age = 90:100) |>
+        ungroup()
     )) |>
-    dplyr::select(-npp_p90) |>
-    dplyr::ungroup() |>
-    dplyr::mutate(data = purrr::map(
-      data, ~ .x |>
-        dplyr::filter(!age_group == "90 and over") |>
-        dplyr::rename(age = age_group) |>
+    left_join(
+      lookup_proj_var |>
+        select(ons_id, id),
+      join_by("ons_id")
+    ) |>
+    left_join(npp_p90, join_by("id")) |>
+    mutate(snpp_p90 = map2(
+      snpp_p90, npp_p90, \(x, y) x |>
+        left_join(y, join_by("year", "sex", "age")) |>
+        group_by(area_code, area_name, sex, year) |>
+        mutate(pop = pop_pct * sum(pop, na.rm = TRUE)) |>
+        ungroup() |>
+        select(-pop_pct)
+    )) |>
+    select(-npp_p90) |>
+    ungroup() |>
+    mutate(data = map(
+      data, \(x) x |>
+        filter(!age_group == "90 and over") |>
+        rename(age = age_group) |>
         mutate(age = as.integer(age))
     )) |>
-    dplyr::mutate(data = purrr::map2(
-      data, snpp_p90, ~ dplyr::bind_rows(.x, .y)
-    )) |>
-    dplyr::select(-snpp_p90)
-    
-    # run some tests
-    # source() code is evaluated in the global environment by default
-    # set local = TRUE to evaluate the code in the calling environment
-    source(here("tests", "testthat", "test-model-syoa-100-snpp-2018b.R"), local = TRUE)
-    
-    # write out
-    readr::write_rds(out_dat, here::here("data", "snpp_2018b_syoa_100.rds"))
+    mutate(data = map2(
+      data, snpp_p90, \(x, y) bind_rows(x, y)
+    ))
+
+  # run some tests
+  # source() code is evaluated in the global environment by default
+  # set local = TRUE to evaluate the code in the calling environment
+  source(here(
+    "tests", "testthat", "test-model-syoa-100-snpp-2018b.R"
+  ), local = TRUE)
+
+  out_dat <- out_dat |>
+    rename(ons_id = ons_id.x) |>
+    select(-snpp_p90, -ons_id.y)
+
+  # save
+  write_rds(out_dat, here("data", "snpp_2018b_syoa_100.rds"))
 }
 
 apply_90plus_dist("npp_2018b.rds", "snpp_2018b.rds")
-
